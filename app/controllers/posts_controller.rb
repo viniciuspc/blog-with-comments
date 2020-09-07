88375d929e268ca782df6d8a78b1bae63aae00f8
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-07 15:29:17 +0100

    Included a before_action in the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :update, :destroy]
+
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -18,11 +20,10 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post
     else
@@ -31,11 +32,10 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
 
     redirect_to posts_path
@@ -46,5 +46,9 @@ class PostsController < ApplicationController
   def post_params
     # Rails only permit the post request send this paramenters
     params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
