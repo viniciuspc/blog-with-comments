3c5d879ec5a1356e371884ce0e631cb4910cc11e
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-07 14:50:57 +0100

    Created comment form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts
+  # Run rake routes on terminal to see available routes
+  resources :posts do
+    # For nested models
+    resources :comments
+  end
   root "posts#index"
 end
